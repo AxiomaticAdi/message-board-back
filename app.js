3a419dc157e3440ba09dd99a04909c5e6644c325
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2024-03-13 14:03:58 -0700

    stable back end can post messages requires reload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const firebase_admin_1 = __importDefault(require("firebase-admin"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 // Use cors middleware to enable CORS
@@ -12,17 +13,54 @@ app.use((0, cors_1.default)());
 const messages = [
     {
         id: 1,
+        username: "testUser",
+        timestamp: new Date().toISOString(),
         content: "Hello, World!",
     },
     {
         id: 2,
+        username: "testUser2",
+        timestamp: new Date().toISOString(),
         content: "Goodbye, World!",
     },
     {
         id: 3,
+        username: "testUser3",
+        timestamp: new Date().toISOString(),
         content: "So long, World!",
     },
 ];
+// Initialize Firebase Admin
+var databaseAdmin = require("firebase-admin");
+var serviceAccount = require("./firebase-admin-sdk-key.json");
+// Initialize the app with a service account, granting admin privileges
+databaseAdmin.initializeApp({
+    credential: firebase_admin_1.default.credential.cert(serviceAccount),
+    databaseURL: "https://messageboard-6ab52-default-rtdb.firebaseio.com",
+});
+const realtimeDb = databaseAdmin.database();
+app.get("/test-realtime-db-write", (req, res) => {
+    const ref = realtimeDb.ref("testPath");
+    ref
+        .set(messages)
+        .then(() => res.send("Data written successfully to Realtime Database."))
+        .catch((error) => {
+        console.error("Error writing to Realtime Database:", error);
+        res.status(500).send("Error writing to Realtime Database");
+    });
+});
+app.get("/test-realtime-db-read", (req, res) => {
+    const ref = realtimeDb.ref("testPath");
+    ref
+        .once("value")
+        .then((snapshot) => {
+        res.json(snapshot.val());
+    })
+        .catch((error) => {
+        console.error("Error reading from Realtime Database:", error);
+        res.status(500).send("Error reading from Realtime Database");
+    });
+});
 // Return messages
 app.get("/", (req, res) => {
     res.json(messages);
